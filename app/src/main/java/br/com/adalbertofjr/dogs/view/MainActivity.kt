1d47eb406986712883e68b75497f4c4fca0f72a8
Adalberto Fernandes Júnior <adalberto.fernandesjr@gmail.com>
2023-01-05 20:11:40 -0300

    Adicionando botão voltar

--- app/src/main/java/br/com/adalbertofjr/dogs/view/MainActivity.kt
+++ app/src/main/java/br/com/adalbertofjr/dogs/view/MainActivity.kt
@@ -2,11 +2,25 @@ package br.com.adalbertofjr.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import br.com.adalbertofjr.dogs.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add button back with navigation controller
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    // Action to button back with navigation controller
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
