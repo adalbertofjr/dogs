127fee696c258c2ee2f151034b6345a462250f67
Adalberto Fernandes Júnior <adalberto.fernandesjr@gmail.com>
2023-01-06 21:22:26 -0300

    Removendo argumentos da DetailFragment

--- app/src/main/java/br/com/adalbertofjr/dogs/view/DetailFragment.kt
+++ app/src/main/java/br/com/adalbertofjr/dogs/view/DetailFragment.kt
@@ -9,11 +9,6 @@ import androidx.navigation.Navigation
 import br.com.adalbertofjr.dogs.R
 import br.com.adalbertofjr.dogs.databinding.FragmentDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [DetailFragment.newInstance] factory method to
